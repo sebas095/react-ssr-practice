73fecf1b2be63d7fc6d926b1bddfc0b010216e77
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-16 03:54:40 -0500

    [ADD] config server

---
+++ api/index.ts
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+
+import express, { Application, Router } from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+
+import home from './src/routes';
+import items from './src/routes/items.routes';
+import search from './src/routes/search.routes';
+
+// Middlewares
+import { ErrorMiddleware, NotFoundMiddleware } from './src/middlewares';
+
+// Config
+dotenv.config();
+const PORT: number = Number(process.env.PORT) || 5000;
+
+const app: Application = express();
+const apiRoutes: Router = Router();
+
+// use middlewares
+apiRoutes.use(cors());
+apiRoutes.use(express.json());
+apiRoutes.use(logger('dev'));
+
+// routes
+home(apiRoutes, '/');
+items(apiRoutes, '/items');
+search(apiRoutes, '/search');
+
+app.use('/v1/api', apiRoutes);
+
+app.use(NotFoundMiddleware);
+app.use(ErrorMiddleware);
+
+app.listen(PORT, () => {
+  console.log(`🚀 API running on port ${PORT}`);
+});
