d5fc897ab83394788c38de7e09ffe2d230f51520
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-20 00:25:38 -0500

    [ADD] search controller and route added

---
+++ api/src/controllers/search.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+
+import { MLibreService } from '../services/mlibre.service';
+import { TError, ErrorType } from '../utils/constants';
+import { formatError } from '../utils/formatter';
+
+class SearchController {
+  async search(req: Request, res: Response) {
+    const { q = '', limit = 50 } = req.query;
+    try {
+      const items: TSearchResponse = await MLibreService.searchItems(
+        q as string,
+        Number(limit),
+      );
+      res.status(200).json(items);
+    } catch (err) {
+      const error: TError = formatError(err, ErrorType.MLIBRE_REQUEST_ERROR);
+      throw error;
+    }
+  }
+}
+
+export default new SearchController();
