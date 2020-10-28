547b564722bd647b50024885174434c01c571966
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-28 02:46:20 -0500

    [ADD] render config

---
+++ client/src/frontend/index.js
@@ -0,0 +1,15 @@
+import '@babel/polyfill';
+import React from 'react';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+const history = createBrowserHistory();
+
+ReactDOM.hydrate(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('app'),
+);
