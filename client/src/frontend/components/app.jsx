87ed50c56e1648f8d8771367b6ff5a1e00e0e2dd
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-10-29 02:02:56 -0500

    [ADD] context providers

--- client/src/frontend/components/app.jsx
+++ client/src/frontend/components/app.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { LoadingContextProvider } from '../context/loading';
+import { SearchItemContextProvider } from '../context/searchItem';
+
 import Layout from './layout/layout';
 import Router from '../router';
 
 const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Router />
-    </Layout>
-  </BrowserRouter>
+  <LoadingContextProvider>
+    <SearchItemContextProvider>
+      <BrowserRouter>
+        <Layout>
+          <Router />
+        </Layout>
+      </BrowserRouter>
+    </SearchItemContextProvider>
+  </LoadingContextProvider>
 );
 
 export default App;
