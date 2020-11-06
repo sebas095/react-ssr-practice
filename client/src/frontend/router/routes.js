a98a21e30ed985debfcd4bd6e8ebb8931be18815
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-11-06 01:54:09 -0500

    [ADD] routes with lazy loading

--- client/src/frontend/router/routes.js
+++ client/src/frontend/router/routes.js
@@ -1,4 +1,12 @@
+import loadable from 'react-loadable-visibility/loadable-components';
+
 import Home from '../pages/home';
+
+const Search = loadable(() => import('../pages/search'), { ssr: true });
+const ItemDetail = loadable(() => import('../pages/item-detail'), {
+  ssr: true,
+});
+const NotFound = loadable(() => import('../pages/not-found'), { ssr: true });
 
 const ROUTES = [
   {
@@ -6,6 +14,25 @@ const ROUTES = [
     key: 'HOME',
     exact: true,
     component: Home,
+    auth: false,
+  },
+  {
+    path: ['/search'],
+    key: 'SEARCH',
+    exact: true,
+    component: Search,
+    auth: false,
+  },
+  {
+    path: ['/items/:id'],
+    key: 'ITEMS',
+    exact: true,
+    component: ItemDetail,
+    auth: false,
+  },
+  {
+    key: 'NOT FOUND',
+    component: NotFound,
     auth: false,
   },
 ];
