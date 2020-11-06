cf0c6d1b2ff8082455ef13c002a3cdd7c04e69c5
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-11-06 01:08:45 -0500

    [ADD] global styles

--- client/src/frontend/components/app.jsx
+++ client/src/frontend/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import { LoadingContextProvider } from '../context/loading';
 import { SearchItemContextProvider } from '../context/searchItem';
@@ -7,9 +8,25 @@ import { SearchItemContextProvider } from '../context/searchItem';
 import Layout from './layout/layout';
 import Router from '../router';
 
+import constants from '../styles/constants';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    background-color: ${constants.background};
+    color: ${constants.black};
+    font-family: 'Nunito Sans', sans-serif;
+  }
+`;
+
 const App = () => (
   <LoadingContextProvider>
     <SearchItemContextProvider>
+      <GlobalStyle />
       <BrowserRouter>
         <Layout>
           <Router />
