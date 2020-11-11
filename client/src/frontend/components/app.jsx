0dbb5f54195531b65a6b99b5400ba88491f656ee
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2020-11-11 00:58:23 -0500

    [UPD] remove border box

--- client/src/frontend/components/app.jsx
+++ client/src/frontend/components/app.jsx
@@ -11,16 +11,35 @@ import Router from '../router';
 import constants from '../styles/constants';
 
 const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-
   body {
     margin: 0;
     background-color: ${constants.background};
     color: ${constants.black};
     font-family: 'Nunito Sans', sans-serif;
   }
+
+  a {
+    text-decoration: none;
+    color: ${constants.gray};
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  
+  li {
+    list-style: none;
+  }
+
+  span[role='img'] {
+    margin-right: 0.25em;
+  }
+
 `;
 
 const App = () => (
